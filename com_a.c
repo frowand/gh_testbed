7f959fb9e43e9b78970d2d15f7dd6446035b6dbf
Frank Rowand <frank.rowand@sony.com>
2018-03-20 16:55:26 -0700

    git_com.c: use add_one() from gh_lib

--- com_a.c
+++ com_a.c
@@ -9,7 +9,21 @@
 
 #include <stdio.h>
 
+#include "gh_lib.h"
+
 int main(int argc, char *argv[])
 {
+	int k;
+
 	printf("program name is >>%s<<\n", argv[0]);
+
+	printf("\n");
+	printf("  k  add_one()\n");
+	printf("---  ---------");
+	printf("\n");
+
+	for (k = 0; k < 10; k++)
+		printf("%3d  %3d\n", k, add_one(k));
+
+	printf("\n");
 }
